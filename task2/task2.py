8bba8459836904f13d7b5186525bd32298f7acec
PNPZ <forsul@bk.ru>
2022-11-15 01:29:49 +0300

    add changes

--- task2/task2.py
+++ task2/task2.py
@@ -1,5 +1,5 @@
-f_one = open(r'C:\Users\maksg\Desktop\PL\task2\file1.txt', 'r')
+f_one = open(r'C:\Users\maksg\Desktop\PL_TZ_NT\task2\file1.txt', 'r')
 print(f_one.read())
-f_two = open(r'C:\Users\maksg\Desktop\PL\task2\file2.txt', 'r')
+f_two = open(r'C:\Users\maksg\Desktop\PL_TZ_NT\task2\file2.txt', 'r')
 print(f_two.read())
 
